d43e15ec2384da2211ed92f4ab94dc159a96b3bd
Sangeetha1105 <62664622+Sangeetha1105@users.noreply.github.com>
2026-03-16 12:09:58 +0100

    feat: add Swagger API  (#1)
    * chore: add .NET gitignore configuration
    
    * docs: add project README
    
    * feat: add Swagger API documentation
    
    * fix: modified swagger doc
    
    ---------
    
    Co-authored-by: Sangeetha <sangeetha@Nivethans-MacBook-Air.local>

--- obj/Debug/net10.0/smart-docu-mind.AssemblyInfo.cs
+++ obj/Debug/net10.0/smart-docu-mind.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smart-docu-mind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aac05cf93693d7fc65a478a101cdfad8a87810a")]
 [assembly: System.Reflection.AssemblyProductAttribute("smart-docu-mind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smart-docu-mind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
